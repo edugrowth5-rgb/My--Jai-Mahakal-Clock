175f4d256acb9426a16f4cca4bf8574b2636b287
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-22 17:52:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'mahakal-clock-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
